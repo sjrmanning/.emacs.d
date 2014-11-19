e7a3a12e3ca044e9f9594419089d493c4b441021
Simon Manning <simon@ecksdee.org>
2014-11-19 13:31:25 +0800

    Smarter default commenting keybind.

--- init/init-bindings.el
+++ init/init-bindings.el
@@ -6,6 +6,9 @@
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
 
+;; Smarter commenting.
+(global-set-key (kbd "M-;") 'comment-or-uncomment-region)
+
 ;; M-x alternative (C-x C-m)
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
