2a0b27f7972ab34a0b48e7bceba29ce332fe166f
Simon Manning <simon@ecksdee.org>
2015-04-17 10:14:18 +0800

    Look/font changes.

--- init/init-look.el
+++ init/init-look.el
@@ -1,5 +1,12 @@
 ;; Display related settings.
 
+(require 'init-defuns)
+
+(defvar sm/fixed-font-name "Office Code Pro")
+(defvar sm/fixed-font-weight 'light)
+(defvar sm/var-font-name "Fira Sans")
+(defvar sm/font-height 150)
+
 ;; Window setup.
 (add-hook 'window-setup-hook
           (lambda nil
@@ -7,21 +14,20 @@
             (set-fringe-mode '(8 . 0))
             (set-face-attribute
              'default nil
-             :family "Fira Mono"
-             :height 140
-             :weight 'normal)
+             :family sm/fixed-font-name
+             :height sm/font-height
+             :weight sm/fixed-font-weight)
             (set-face-attribute
              'linum nil
-             :family "Fira Mono"
-             :height 120
-             :weight 'normal)
+             :family sm/fixed-font-name
+             :height (- sm/font-height 20)
+             :weight sm/fixed-font-weight)
             (set-face-attribute
              'variable-pitch nil
-             :family "Fira Sans")))
+             :family sm/var-font-name)))
 
 ;; Custom themes path.
-(add-to-list 'custom-theme-load-path
-             (expand-file-name "etc/themes/" user-emacs-directory))
+(add-to-list 'custom-theme-load-path (sm/emacs.d "etc/themes"))
 
 ;; Disable cursor display in inactive windows.
 (setq-default cursor-in-non-selected-windows nil)
