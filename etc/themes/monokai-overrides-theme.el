3d9dbae40aa1f7ff348d1bb70f69f887f66ba51c
Simon Manning <simon@ecksd.com>
2015-06-15 17:03:55 +0800

    Fix linum and inactive mode-line issues in terminal due to monokai customisations.

--- etc/themes/monokai-overrides-theme.el
+++ etc/themes/monokai-overrides-theme.el
@@ -1,58 +1,61 @@
 (deftheme monokai-overrides)
 
-(custom-theme-set-faces
- 'monokai-overrides
+(let ((class '((class color) (min-colors 257)))
+      (terminal-class '((class color) (min-colors 89))))
 
- ;; Minor tweaks
- '(linum
-   ((t :foreground "#75715E"
-       :background "#49483E")))
- '(mode-line-inactive
-   ((t (:box (:line-width 1 :color "#2c2d26" :style nil)
-             :background "#2c2d26"))))
+  (custom-theme-set-faces
+   'monokai-overrides
 
- ;; Additional modes
- ;; Company tweaks.
- '(company-tooltip-common
-   ((t :foreground "#F8F8F0"
-       :background "#474747"
-       :underline t)))
+   ;; Linum and mode-line improvements (only in sRGB).
+   `(linum
+     ((,class :foreground "#75715E"
+              :background "#49483E")))
+   `(mode-line-inactive
+     ((,class (:box (:line-width 1 :color "#2c2d26" :style nil)
+                    :background "#2c2d26"))))
 
- '(company-tooltip-common-selection
-   ((t :foreground "#F8F8F0"
-       :background "#474747"
-       :underline t)))
+   ;; Additional modes
+   ;; Company tweaks.
+   `(company-tooltip-common
+     ((t :foreground "#F8F8F0"
+         :background "#474747"
+         :underline t)))
 
- '(company-scrollbar-fg
-   ((t :background "#BBF7EF")))
+   `(company-tooltip-common-selection
+     ((t :foreground "#F8F8F0"
+         :background "#474747"
+         :underline t)))
 
- '(company-tooltip-annotation
-   ((t :inherit company-tooltip
-       :foreground "#C2A1FF")))
+   `(company-scrollbar-fg
+     ((t :background "#BBF7EF")))
 
- ;; Popup menu tweaks.
- '(popup-menu-face
-   ((t :foreground "#A1EFE4"
-       :background "#49483E")))
+   `(company-tooltip-annotation
+     ((t :inherit company-tooltip
+         :foreground "#C2A1FF")))
 
- '(popup-menu-selection-face
-   ((t :background "#349B8D"
-       :foreground "#BBF7EF")))
+   ;; Popup menu tweaks.
+   `(popup-menu-face
+     ((t :foreground "#A1EFE4"
+         :background "#49483E")))
 
- ;; Circe
- '(circe-prompt-face
-   ((t (:foreground "#C2A1FF" :weight bold))))
+   `(popup-menu-selection-face
+     ((t :background "#349B8D"
+         :foreground "#BBF7EF")))
 
- '(circe-server-face
-   ((t (:foreground "#75715E"))))
+   ;; Circe
+   `(circe-prompt-face
+     ((t (:foreground "#C2A1FF" :weight bold))))
 
- '(circe-highlight-nick-face
-   ((t (:foreground "#AE81FF" :weight bold))))
+   `(circe-server-face
+     ((t (:foreground "#75715E"))))
 
- '(circe-my-message-face
-   ((t (:foreground "#E6DB74"))))
+   `(circe-highlight-nick-face
+     ((t (:foreground "#AE81FF" :weight bold))))
 
- '(circe-originator-face
-   ((t (:weight bold)))))
+   `(circe-my-message-face
+     ((t (:foreground "#E6DB74"))))
 
-(provide-theme 'monokai-overrides)
+   `(circe-originator-face
+     ((t (:weight bold))))))
+
+(provide-theme `monokai-overrides)
