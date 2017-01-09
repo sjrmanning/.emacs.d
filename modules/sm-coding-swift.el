facb46acb268a77720ac2f2ea4328366667a9a46
Simon Manning <simon@ecksd.com>
2017-01-09 15:42:36 +0700

    Swift mode fixes.

--- modules/sm-coding-swift.el
+++ modules/sm-coding-swift.el
@@ -5,8 +5,11 @@
   :commands swift-mode
   :mode ("\\.swift\\'" . swift-mode)
   :config
+  (ycmd-mode -1)
   (use-package company-sourcekit
+    :init (setq company-sourcekit-use-yasnippet t)
     :config
+    (setq sourcekit-verbose t)
     (add-to-list 'company-backends 'company-sourcekit)))
 
 (provide 'sm-coding-swift)
