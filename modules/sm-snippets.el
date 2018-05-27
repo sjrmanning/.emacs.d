e0101c7d6f780dee3cb7579a59d3f89b16ac26c5
George Hartzell <hartzell@alerce.com>
2018-05-26 18:51:55 -0700

    Fix up the snippets config
    - ensure that the “yas-installed-snippets-dir” exists

--- modules/sm-snippets.el
+++ modules/sm-snippets.el
@@ -7,7 +7,14 @@
   (defvar custom-snippets-dir (sm/emacs.d "etc/snippets/"))
   (sm/mkdir-p custom-snippets-dir)
   ;; Replace default custom dir with our own.
-  (setq yas-snippet-dirs '(custom-snippets-dir))
+  (setq yas-snippet-dirs '(custom-snippets-dir
+                           yas-installed-snippets-dir))
+  ;; Suppress excessive log messages
+  (setq yas-verbosity 1)
+  :config
+  ;; Ensure that this dir exists, avoid warning.
+  (sm/mkdir-p yas-installed-snippets-dir)
+  (yas-global-mode t)
   ;; Disable yasnippet in some modes.
   (defun yas-disable-hook ()
     (setq yas-dont-activate t))
