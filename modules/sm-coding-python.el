a3aff8053089e9c1c05797c2578bbc156292016d
hartzell <hartzell@alerce.com>
2018-08-23 18:05:19 -0700

    Hook up python format-er (yapf)
    use-package and add hook

--- modules/sm-coding-python.el
+++ modules/sm-coding-python.el
@@ -8,4 +8,14 @@
     :init (require 'lsp-python-ms)
     :hook (python-mode . lsp)))
 
+;; Clone yapf somewhere and throw a script like this onto your bin
+;; (sigh...)
+;;
+;; #!/bin/sh
+;; PYTHONPATH=$HOME/tmp/yapf python3 $HOME/tmp/yapf/yapf "$@"
+(use-package py-yapf
+  :config
+  (add-hook 'python-mode-hook 'py-yapf-enable-on-save))
+
+
 (provide 'sm-coding-python)
