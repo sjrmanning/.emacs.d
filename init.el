db346056374aa594633da0d96751f7bb88bf5e00
Simon Manning <smanning@galileo.io>
2019-06-06 16:52:54 -0400

    Smarter GC

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
 ;;; init.el --- Start of the Emacs initialisation process.
 
 ;; Increase the GC threshold as soon as possible.
-(setq gc-cons-threshold 50000000)
+(setq gc-cons-threshold 402653184
+      gc-cons-percentage 1.0)
+(add-hook 'after-init-hook
+          (lambda ()
+            (setq gc-cons-threshold 16777216)))
 
 ;; Not using package.el.
 (setq package-enable-at-startup nil)
