e67b109650eca66c23d219d62d72619731eb5595
hartzell <hartzell@alerce.com>
2019-08-27 20:11:06 -0700

    First cut LSP and gopls setup
    - https://github.com/emacs-lsp/lsp-mode
    - https://github.com/golang/go/wiki/gopls
    - https://utcc.utoronto.ca/~cks/space/blog/programming/GoEmacsWithLspMode

--- modules/sm-coding-go.el
+++ modules/sm-coding-go.el
@@ -8,10 +8,30 @@
   (setq gofmt-command "goimports")
   (add-hook 'before-save-hook 'gofmt-before-save)
   (add-hook 'go-mode-hook (lambda ()
+                            ;; (flycheck-mode)
                             (local-set-key (kbd "M-.") 'godef-jump)
                             (local-set-key (kbd "M-*") 'pop-tag-mark))))
 
 (use-package go-guru)
+
+;;;;;;;
+
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :config
+  (add-hook 'go-mode-hook #'lsp-deferred))
+
+;; optional - provides fancier overlays
+;; (use-package lsp-ui
+;;   :commands lsp-ui-mode
+;;   :config
+;;   (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+;; (use-package yasnippet)
+
+;; if you use company-mode for completion (otherwise,
+;; complete-at-point works out of the box):
+;; (use-package company-lsp
+;;   :commands company-lsp)
 
 (provide 'sm-coding-go)
 
