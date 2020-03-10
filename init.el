b0be6c6e3d09a82380b1b79bb4c9fdc4fe6eea37
Simon Manning <smanning@galileo.io>
2020-03-10 12:08:48 -0400

    init updates for early-init

--- init.el
+++ init.el
@@ -1,14 +1,9 @@
 ;;; init.el --- Start of the Emacs initialisation process.
 
-;; Increase the GC threshold as soon as possible.
-(setq gc-cons-threshold 402653184
-      gc-cons-percentage 1.0)
+;; Reset GC threshold after init to something reasonable.
 (add-hook 'after-init-hook
           (lambda ()
             (setq gc-cons-threshold 16777216)))
-
-;; Not using package.el.
-(setq package-enable-at-startup nil)
 
 ;; Prepare paths.
 (add-to-list 'load-path (expand-file-name "core/" user-emacs-directory))
