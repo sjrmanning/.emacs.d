3e8976626f335edfaf5640f4a7c9b9c1b94469e2
Simon Manning <smanning@galileo.io>
2020-06-14 11:04:47 -0400

    Use inbuilt Xcode SourceKit path for LSP.

--- modules/sm-coding-swift.el
+++ modules/sm-coding-swift.el
@@ -10,12 +10,12 @@
   :hook (swift-mode . flycheck-swift-setup)
   :config
   (setq flycheck-swift-sdk-path "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk"
-        flycheck-swift-target "arm64-apple-ios13.0"))
+        flycheck-swift-target "arm64-apple-ios13.4"))
 
 (use-package lsp-sourcekit
   :after lsp-mode
   :config
-  (setenv "SOURCEKIT_TOOLCHAIN_PATH" "/Library/Developer/Toolchains/swift-5.1.4-RELEASE.xctoolchain")
-  (setq lsp-sourcekit-executable "/Library/Developer/Toolchains/swift-5.1.4-RELEASE.xctoolchain/usr/bin/sourcekit-lsp"))
+  (setenv "SOURCEKIT_TOOLCHAIN_PATH" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain")
+  (setq lsp-sourcekit-executable "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/sourcekit-lsp"))
 
 (provide 'sm-coding-swift)
