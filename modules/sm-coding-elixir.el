61e9b27b792876fc8fb0636fa103a362da29781e
Simon Manning <smanning@galileo.io>
2020-07-11 16:21:18 -0400

    Replaced alchemist with elixir-ls.

--- modules/sm-coding-elixir.el
+++ modules/sm-coding-elixir.el
@@ -1,11 +1,9 @@
 ;;; sm-coding-elixir.el --- Elixir configuration.
 
-(use-package alchemist
-  :after elixir-mode
-  :hook (elixir-mode . alchemist-mode)
-  :hook (elixir-mode . flycheck-mode))
-
 (use-package elixir-mode
-  :interpreter "elixir")
+  :interpreter "elixir"
+  :hook (elixir-mode . lsp)
+  :custom (lsp-clients-elixir-server-executable
+           "/usr/local/bin/elixir-ls/language_server.sh"))
 
 (provide 'sm-coding-elixir)
