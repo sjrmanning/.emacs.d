518ced5e7ae3f112c4e7d574f6c0555db651fa9e
hartzell <hartzell@alerce.com>
2020-11-12 14:04:24 -0800

    Hook up selectrum

--- modules/sm-ui.el
+++ modules/sm-ui.el
@@ -7,42 +7,21 @@
 ;; Overrides Emacs' default mechanism for making buffer names unique.
 (setq uniquify-buffer-name-style 'forward)
 
+(use-package prescient
+  :hook
+  (after-init . prescient-persist-mode))
+(use-package selectrum-prescient
+  :hook
+  (after-init . selectrum-prescient-mode))
+
+(use-package selectrum
+  :hook
+  (after-init . selectrum-mode)
+  )
+
 ;; smex support in counsel
 (use-package smex
   :hook (after-init . smex-initialize))
-
-;; fuzzy matching and better sorting for ivy.
-(use-package ivy-prescient
-  :hook (after-init . ivy-prescient-mode)
-  :config
-  (setq prescient-filter-method 'fuzzy))
-
-;; ivy everywhere
-(use-package ivy
-  :commands ivy-mode
-  :hook (after-init . ivy-mode)
-  :delight ivy-mode
-  :bind (:map ivy-minibuffer-map
-              ("C-j" . ivy-immediate-done)
-              ("RET" . ivy-alt-done))
-  :config
-  (setq ivy-use-virtual-buffers t
-        ivy-virtual-abbreviate 'full
-        ivy-on-del-error-function nil
-        ivy-use-selectable-prompt t
-        enable-recursive-minibuffers t
-        ivy-initial-inputs-alist nil
-        ivy-re-builders-alist
-        '((counsel-ag . ivy--regex-plus)
-          (counsel-rg . ivy--regex-plus)
-          (t      . ivy-prescient-re-builder))))
-
-(use-package counsel
-  :hook (after-init . counsel-mode)
-  :delight counsel-mode
-  :bind (("M-x" . counsel-M-x)
-         ("C-x C-f" . counsel-find-file)
-         ("C-c s" . counsel-rg)))
 
 ;; diminish some modes.
 (use-package simple
