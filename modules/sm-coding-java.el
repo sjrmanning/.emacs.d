4c4c518fd520c305d921c7b3fb1bc5e947cb09b9
george.hartzell <george.hartzell@sana.com>
2022-06-05 11:22:37 -0600

    Disable ensime mode
    - it's gone missing and I don't care....

--- modules/sm-coding-java.el
+++ modules/sm-coding-java.el
@@ -1,8 +1,8 @@
 ;;; sm-coding-java.el --- Java and Android config.
 
-(use-package ensime
-  :straight (:host github :repo "ensime/ensime-emacs" :branch "2.0")
-  :commands java-mode)
+;; (use-package ensime
+;;   :straight (:host github :repo "ensime/ensime-emacs" :branch "2.0")
+;;   :commands java-mode)
 
 (use-package java
   :straight java-imports
@@ -10,14 +10,15 @@
   :config
   (setq java-imports-find-block-function 'java-imports-find-place-sorted-block)
   :init
-  (add-hook 'java-mode-hook
-            (lambda ()
-              (require 'ensime)
-              (ensime-mode t)
-              (java-imports-scan-file)
-              (bind-key "M-I" 'java-imports-add-import-dwim java-mode-map)
-              (setq tab-width 2
-                    c-basic-offset 2))))
+  ;; (add-hook 'java-mode-hook
+  ;;           (lambda ()
+  ;;             (require 'ensime)
+  ;;             (ensime-mode t)
+  ;;             (java-imports-scan-file)
+  ;;             (bind-key "M-I" 'java-imports-add-import-dwim java-mode-map)
+  ;;             (setq tab-width 2
+  ;;                   c-basic-offset 2)))
+  )
 
 (use-package kotlin-mode
   :commands kotlin-mode
